494b3c620c76526407e04c76622e267d04720924
tle <toan.lee@outlook.com>
2016-05-18 16:50:32 +0700

    toogle Left menu bar

--- DashboardTemplates/DashboardOne/app/dashboard/controllers/navbar/leftmenuController.js
+++ DashboardTemplates/DashboardOne/app/dashboard/controllers/navbar/leftmenuController.js
@@ -3,145 +3,147 @@
     var leftmenuController = function ($scope, $location) {
 
         var viewModel = function () {
-            // Init Left Menu
-            $(function () { $('#side-menu').metisMenu(); });
-            $(window).bind("load resize", function () {
-                var topOffset = 50;
-                var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-                if (width < 768) {
-                    $('div.navbar-collapse').addClass('collapse');
-                    topOffset = 100; // 2-row-menu
-                } else {
-                    $('div.navbar-collapse').removeClass('collapse');
-                }
 
-                var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
-                height = height - topOffset;
-                if (height < 1) height = 1;
-                if (height > topOffset) {
-                    $("#page-wrapper").css("min-height", (height) + "px");
-                }
-            });
+            /* PushMenu()
+            * ==========
+            * Adds the push menu functionality to the sidebar.
+            *
+            * @type Function
+            * @usage: $.AdminLTE.pushMenu("[data-toggle='offcanvas']")
+            */
+            var pushMenu = {
+                activate: function(toggleBtn) {
+                    //Get the screen sizes
+                    //var screenSizes = options.screenSizes;
 
-            //var url = window.location;
-            //var element = $('ul.nav a').filter(function () {
-            //    return this.href == url || url.href.indexOf(this.href) == 0;
-            //}).addClass('active').parent().parent().addClass('in').parent();
-            //if (element.is('li')) {
-            //    element.addClass('active');
-            //}
+                    //Enable sidebar toggle
+                    //$(toggleBtn).on('click', function(e) {
+                    //    e.preventDefault();
 
-            var options = {
-                //Add slimscroll to navbar menus
-                //This requires you to load the slimscroll plugin
-                //in every page before app.js
-                navbarMenuSlimscroll: true,
-                navbarMenuSlimscrollWidth: "3px", //The width of the scroll bar
-                navbarMenuHeight: "200px", //The height of the inner menu
-                //General animation speed for JS animated elements such as box collapse/expand and
-                //sidebar treeview slide up/down. This options accepts an integer as milliseconds,
-                //'fast', 'normal', or 'slow'
-                animationSpeed: 500,
-                //Sidebar push menu toggle button selector
-                sidebarToggleSelector: "[data-toggle='offcanvas']",
-                //Activate sidebar push menu
-                sidebarPushMenu: true,
-                //Activate sidebar slimscroll if the fixed layout is set (requires SlimScroll Plugin)
-                sidebarSlimScroll: true,
-                //Enable sidebar expand on hover effect for sidebar mini
-                //This option is forced to true if both the fixed layout and sidebar mini
-                //are used together
-                sidebarExpandOnHover: false,
-                //BoxRefresh Plugin
-                enableBoxRefresh: true,
-                //Bootstrap.js tooltip
-                enableBSToppltip: true,
-                BSTooltipSelector: "[data-toggle='tooltip']",
-                //Enable Fast Click. Fastclick.js creates a more
-                //native touch experience with touch devices. If you
-                //choose to enable the plugin, make sure you load the script
-                //before AdminLTE's app.js
-                enableFastclick: true,
-                //Control Sidebar Options
-                enableControlSidebar: true,
-                controlSidebarOptions: {
-                    //Which button should trigger the open/close event
-                    toggleBtnSelector: "[data-toggle='control-sidebar']",
-                    //The sidebar selector
-                    selector: ".control-sidebar",
-                    //Enable slide over content
-                    slide: true
-                },
-                //Box Widget Plugin. Enable this plugin
-                //to allow boxes to be collapsed and/or removed
-                enableBoxWidget: true,
-                //Box Widget plugin options
-                boxWidgetOptions: {
-                    boxWidgetIcons: {
-                        //Collapse icon
-                        collapse: 'fa-minus',
-                        //Open icon
-                        open: 'fa-plus',
-                        //Remove icon
-                        remove: 'fa-times'
-                    },
-                    boxWidgetSelectors: {
-                        //Remove button selector
-                        remove: '[data-widget="remove"]',
-                        //Collapse button selector
-                        collapse: '[data-widget="collapse"]'
+                    //    //Enable sidebar push menu
+                    //    if ($(window).width() > (screenSizes.sm - 1)) {
+                    //        if ($("body").hasClass('sidebar-collapse')) {
+                    //            $("body").removeClass('sidebar-collapse').trigger('expanded.pushMenu');
+                    //        } else {
+                    //            $("body").addClass('sidebar-collapse').trigger('collapsed.pushMenu');
+                    //        }
+                    //    }
+                    //        //Handle sidebar push menu for small screens
+                    //    else {
+                    //        if ($("body").hasClass('sidebar-open')) {
+                    //            $("body").removeClass('sidebar-open').removeClass('sidebar-collapse').trigger('collapsed.pushMenu');
+                    //        } else {
+                    //            $("body").addClass('sidebar-open').trigger('expanded.pushMenu');
+                    //        }
+                    //    }
+                    //});
+
+                    $(".content-wrapper").click(function() {
+                        //Enable hide menu when clicking on the content-wrapper on small screens
+                        if ($(window).width() <= (AppConfig.screenSizes.sm - 1) && $("body").hasClass("sidebar-open")) {
+                            $("body").removeClass('sidebar-open');
+                        }
+                    });
+
+                    //Enable expand on hover for sidebar mini
+                    if (AppConfig.sidebarExpandOnHover || ($('body').hasClass('fixed') && $('body').hasClass('sidebar-mini'))) {
+                        this.expandOnHover();
                     }
                 },
-                //Direct Chat plugin options
-                directChat: {
-                    //Enable direct chat by default
-                    enable: true,
-                    //The button to open and close the chat contacts pane
-                    contactToggleSelector: '[data-widget="chat-pane-toggle"]'
+                expandOnHover: function() {
+                    var _this = this;
+                    var screenWidth = AppConfig.screenSizes.sm - 1;
+                    //Expand sidebar on hover
+                    $('.main-sidebar').hover(function() {
+                        if ($('body').hasClass('sidebar-mini')
+                            && $("body").hasClass('sidebar-collapse')
+                            && $(window).width() > screenWidth) {
+                            _this.expand();
+                        }
+                    }, function() {
+                        if ($('body').hasClass('sidebar-mini')
+                            && $('body').hasClass('sidebar-expanded-on-hover')
+                            && $(window).width() > screenWidth) {
+                            _this.collapse();
+                        }
+                    });
                 },
-                //Define the set of colors to use globally around the website
-                colors: {
-                    lightBlue: "#3c8dbc",
-                    red: "#f56954",
-                    green: "#00a65a",
-                    aqua: "#00c0ef",
-                    yellow: "#f39c12",
-                    blue: "#0073b7",
-                    navy: "#001F3F",
-                    teal: "#39CCCC",
-                    olive: "#3D9970",
-                    lime: "#01FF70",
-                    orange: "#FF851B",
-                    fuchsia: "#F012BE",
-                    purple: "#8E24AA",
-                    maroon: "#D81B60",
-                    black: "#222222",
-                    gray: "#d2d6de"
+                expand: function() {
+                    $("body").removeClass('sidebar-collapse').addClass('sidebar-expanded-on-hover');
                 },
-                //The standard screen sizes that bootstrap uses.
-                //If you change these in the variables.less file, change
-                //them here too.
-                screenSizes: { xs: 480, sm: 768, md: 992, lg: 1200 }
+                collapse: function() {
+                    if ($('body').hasClass('sidebar-expanded-on-hover')) {
+                        $('body').removeClass('sidebar-expanded-on-hover').addClass('sidebar-collapse');
+                    }
+                }
             };
 
+            /* Tree()
+            * ======
+            * Converts the sidebar into a multilevel
+            * tree view menu.
+            *
+            * @type Function
+            * @Usage: $.AdminLTE.tree('.sidebar')
+            */
+            var tree = function (menu) {
+                var _this = this;
+                var animationSpeed = $.AdminLTE.options.animationSpeed;
+                $(document).on('click', menu + ' li a', function (e) {
+                    //Get the clicked link and the next element
+                    var $this = $(this);
+                    var checkElement = $this.next();
 
+                    //Check if the next element is a menu and is visible
+                    if ((checkElement.is('.treeview-menu')) && (checkElement.is(':visible'))) {
+                        //Close the menu
+                        checkElement.slideUp(animationSpeed, function () {
+                            checkElement.removeClass('menu-open');
+                            //Fix the layout in case the sidebar stretches over the height of the window
+                            //_this.layout.fix();
+                        });
+                        checkElement.parent("li").removeClass("active");
+                    }
+                        //If the menu is not visible
+                    else if ((checkElement.is('.treeview-menu')) && (!checkElement.is(':visible'))) {
+                        //Get the parent menu
+                        var parent = $this.parents('ul').first();
+                        //Close all open menus within the parent
+                        var ul = parent.find('ul:visible').slideUp(animationSpeed);
+                        //Remove the menu-open class from the parent
+                        ul.removeClass('menu-open');
+                        //Get the parent li
+                        var parent_li = $this.parent("li");
 
+                        //Open the target menu and add the menu-open class
+                        checkElement.slideDown(animationSpeed, function () {
+                            //Add the class active to the parent li
+                            checkElement.addClass('menu-open');
+                            parent.find('li.active').removeClass('active');
+                            parent_li.addClass('active');
+                            //Fix the layout in case the sidebar stretches over the height of the window
+                            _this.layout.fix();
+                        });
+                    }
+                    //if this isn't a link, prevent the page from being redirected
+                    if (checkElement.is('.treeview-menu')) {
+                        e.preventDefault();
+                    }
+                });
+            };
 
-
-            var init = function() {
+            var init = function () {
+                pushMenu.activate(options.sidebarToggleSelector);
             };
 
             return {
-                Init: init,
-                Active: function (event) {
-                    var activeElement = $('a.active');
-                    activeElement.removeClass('active');
-
-                    var currentElement = $(event.currentTarget);
-                    currentElement.addClass('active');
-                }
+                Init: init
             };
         }();
+
+        $scope.$onRootScope('someComponent.someCrazyEvent', function () {
+            console.log('foo');
+        });
 
         $scope.leftmenuViewModel = viewModel;
     };
--- DashboardTemplates/DashboardOne/app/dashboard/main.js
+++ DashboardTemplates/DashboardOne/app/dashboard/main.js
@@ -6,7 +6,7 @@
     spa.dashboardApp = angular.module('dashboardApp', ['ngRoute', 'ui.bootstrap']);
 
     // Global Configuration
-    spa.dashboardApp.config(function ($routeProvider, $locationProvider, $httpProvider) {
+    spa.dashboardApp.config(function ($routeProvider, $locationProvider, $httpProvider, $provide) {
         $routeProvider
             .when('/Dashboard', {
                 templateUrl: Helpers.rootUrl + 'app/admin/views/_common/dashboard.html',
@@ -16,6 +16,23 @@
         $locationProvider.html5Mode(true);
         $httpProvider.defaults.timeout = 60000;
         //$httpProvider.interceptors.push('AppInterceptor');
+        
+        // http://stackoverflow.com/questions/11252780/whats-the-correct-way-to-communicate-between-controllers-in-angularjs
+        $provide.decorator('$rootScope', ['$delegate', function ($delegate) {
+
+            Object.defineProperty($delegate.constructor.prototype, '$onRootScope', {
+                value: function (name, listener) {
+                    var unsubscribe = $delegate.$on(name, listener);
+                    this.$on('$destroy', unsubscribe);
+
+                    return unsubscribe;
+                },
+                enumerable: false
+            });
+
+
+            return $delegate;
+        }]);
     });
 
     spa.dashboardApp.run(function () {
